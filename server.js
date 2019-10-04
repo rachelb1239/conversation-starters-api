0b7a87c58ac42df93af5a37fc49163b8ad5bd5b7
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-10-04 14:08:28 -0600

    questions routes setup

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Middleware
+app.use(express.json()); // parse json
+app.use(morgan('tiny')); // logging
 
+// Routes
+app.use('/', require('./server/routes')); 
+
+// Listen for Connections
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
